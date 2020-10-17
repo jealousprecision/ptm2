254622f371616df1dcff3ff440ca87cf9d14fdb1
jealousprecision <maksymilian.kadukowski@gmail.com>
2020-10-17 14:58:47 +0200

    wstep done

---
+++ zaj1/wstep/main.c
@@ -0,0 +1,31 @@
+#define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <string.h>
+#include <util/delay.h>
+
+#ifndef _BV
+#define _BV(bit)				(1<<(bit))
+#endif
+
+#ifndef sbi
+#define sbi(reg,bit)		reg |= (_BV(bit))
+#endif
+
+#ifndef cbi
+#define cbi(reg,bit)		reg &= ~(_BV(bit))
+#endif
+
+int main()
+{
+	sbi(DDRD, PD6);
+
+	while (1)
+    {
+		sbi(PORTD, PD6);
+		_delay_ms(500);
+		cbi(PORTD, PD6);
+		_delay_ms(500);
+	}
+}
